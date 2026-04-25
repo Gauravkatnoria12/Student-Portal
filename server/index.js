2c8cd78912aaed457e10c82df64b9a9f853006d3
Gauravkatnoria12 <144324982+Gauravkatnoria12@users.noreply.github.com>
2026-04-25 16:16:29 +0530

    fix: add missing path module

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const { User, Student, Attendance, Fees, initDb } = require('./database');
 require('dotenv').config();
 
